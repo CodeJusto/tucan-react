d204b615df7996e18a301974aff36e1238940ad7
Justin Wong <justincwong89@gmail.com>
2016-06-18 14:01:52 -0700

    Added gateway feature to prevent unwanted access if not logged in

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,12 +7,22 @@ export default class Navbar extends Component {
   // Original JavaScript code by Chirp Internet: www.chirp.com.au
   // Please acknowledge use of this code by including this header.
 
+  // var login() {
+  //   if (getCookie('username') == nil)
+  // }
   
   render() {
 
+        var cookie = function getCookie() {
+      var re = new RegExp('user_name' + "=([^;]+)");
+      var value = re.exec(document.cookie);
+      return ((value != null) ? "You are now signed in as " + unescape(value[1]) : "You do not have access to this page").replace('+', ' ')
+      
+    }
+   
     return (
       <div className="navbar">
-       <User />
+       <User userCheck={cookie()}/>
       </div>
       )
   }
--- src/components/navbar/user.jsx
+++ src/components/navbar/user.jsx
@@ -4,14 +4,9 @@ import $ from 'jquery';
 export default class User extends Component {
 
   render() {
-    function getCookie(name) {
-      var re = new RegExp(name + "=([^;]+)");
-      var value = re.exec(document.cookie);
-      return (value != null) ? unescape(value[1]) : null;
-    }
     return (
-      <div class="user">
-        <p>You are signed in as <strong>{getCookie('user_name')}</strong></p>
+      <div className="user">
+        <p><strong>{this.props.userCheck}</strong></p>
         <a href="http://localhost:4000/signout">Sign out?</a>
       </div>
       )
