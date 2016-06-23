86eb4de0480d27ca7c54d375cac53b4d385ee79b
ceebs <cthiessen@gmail.com>
2016-06-22 23:07:28 -0700

    removed add products when payments have begun

--- src/components/cart/products/productbox.jsx
+++ src/components/cart/products/productbox.jsx
@@ -5,7 +5,7 @@ import $ from 'jquery';
 export default class ProductBox extends Component {
   render() {
     const productBundle = this.props.products.map(product => <Product cart_id={this.props.cart_id} user_id={this.props.user_id} key={product.id} product={product} organizer_id={this.props.organizer_id}/>);
-    if (this.props.products.length > 0) {
+    if (this.props.products.length > 0 && this.props.status === 2) {
       return (
         <div id="products">
           <ul id="product-list" className="collection">
@@ -13,9 +13,17 @@ export default class ProductBox extends Component {
           </ul>
           <a href="#" className="modal-btn" data-modal="add-product-modal"><i className="material-icons">add_circle</i> Add another product</a>
         </div>
-
       )
-    } else {
+    } else if (this.props.products.length > 0 && this.props.payments.length > 0) {
+      return (
+        <div id="products">
+          <ul id="product-list" className="collection">
+            {productBundle}
+          </ul>
+        </div>
+      )
+    }
+    else {
       return (
         <div id="products" className="cart-options">
           <p className="flow-text">Nothing to see here</p>
